cee2d6c6eee1f3b9f1091b25967a165a3f785e85
Ritesh Sil <sil.ritzzz@gmail.com>
2024-03-05 23:44:04 +0530

    setup.py and requirements.txt updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='us-visa',
+    version=1.0,
+    author='Ritesh Sil',
+    author_email='sil.ritzzz@gmail.com',
+    packages=find_packages()
+    )
